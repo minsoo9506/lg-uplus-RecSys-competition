79efe0165a2463efc76588414d7b6dfe7e9e85d4
minsoo9506 <ghktjd15gh@gmail.com>
2022-11-15 00:02:57 +0900

    feat: ndcgk 구현

--- src/utils/score.py
+++ src/utils/score.py
@@ -1,12 +1,14 @@
 from typing import List
 
+import numpy as np
 
-def recallk(actual: List[int], pred: List[int], k: int = 25) -> float:
+
+def recallk(true: List[int], pred: List[int], k: int = 25) -> float:
     """recall k
 
     Parameters
     ----------
-    actual : List[int]
+    true : List[int]
         true item
     pred : List[int]
         prediction item
@@ -18,12 +20,12 @@ def recallk(actual: List[int], pred: List[int], k: int = 25) -> float:
     float
         recall k
     """
-    set_actual = set(actual)
-    recall_k = len(set_actual & set(pred[:k])) / min(k, len(set_actual))
+    set_true = set(true)
+    recall_k = len(set_true & set(pred[:k])) / min(k, len(set_true))
     return recall_k
 
 
-def unique(sequence: List[int]) -> List[int]:
+def get_order_preserved_unique(sequence: List[int]) -> List[int]:
     """unique (preserve list item order)
 
     Parameters
@@ -40,4 +42,29 @@ def unique(sequence: List[int]) -> List[int]:
     return [x for x in sequence if not (x in seen or seen.add(x))]
 
 
-# ndcgk 관련해서 README에 수식 정리
+def ndcgk(true: List[int], pred: List[int], k: int = 25) -> float:
+    """NDCG@K
+
+    Parameters
+    ----------
+    true : List[int]
+        _description_
+    pred : List[int]
+        _description_
+    k : int, optional
+        _description_, by default 25
+
+    Returns
+    -------
+    float
+        ndcg_k value
+    """
+    set_true = set(true)
+    idcg = sum([1.0 / np.log(i + 2) for i in range(min(k, len(set_true)))])
+    dcg = 0.0
+    unique_pred = get_order_preserved_unique(pred[:k])
+    for i, r in enumerate(unique_pred):
+        if r in set_true:
+            dcg += 1.0 / np.log(i + 2)
+    ndcg_k = dcg / idcg
+    return ndcg_k
